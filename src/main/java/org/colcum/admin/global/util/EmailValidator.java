41a9b8fe0735fb2bf5bef90866c378bf52d008cd
sirin0762 <tlsdydwls123@gmail.com>
2024-03-11 20:35:52 +0900

    feat: base entity 추가
    모든 테이블에 추가될 메타데이터를 base entity를 통해 추가하였다.
    메타데이터의 목록은 아래와 같다.
    메타데이터 : isDeleted / createdAt / createdBy / modifiedAt / modifiedBy

---
+++ src/main/java/org/colcum/admin/global/util/EmailValidator.java
@@ -0,0 +1,27 @@
+package org.colcum.admin.global.util;
+
+import org.colcum.admin.global.Error.EmailValidationException;
+import org.springframework.util.StringUtils;
+
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    public static void validate(String email) {
+        if (!StringUtils.hasText(email)) {
+            throw new EmailValidationException("Email must not be empty.");
+        }
+
+        if (email.length() <= 50) {
+            throw new EmailValidationException("Email is too long.");
+        }
+
+        if (EMAIL_PATTERN.matcher(email).matches()) {
+            throw new EmailValidationException("Invalid email format.");
+        }
+    }
+
+}
