170f0830237bf79b533f27a6ca9937607224686a
sirin0762 <tlsdydwls123@gmail.com>
2024-03-25 20:55:35 +0900

    chore: querydsl 추가
    동적 쿼리 적용을 위하여 쿼리 dsl을 추가하였다.

--- src/main/java/org/colcum/admin/global/common/config/JpaConfiguration.java
+++ src/main/java/org/colcum/admin/global/common/config/JpaConfiguration.java
@@ -1,18 +1,11 @@
 package org.colcum.admin.global.common.config;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
@@ -21,6 +14,11 @@ public class JpaConfiguration {
     @Bean
     public AuditorAware<String> auditorAware() {
         return new SecurityAuditorAware();
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 
 }
