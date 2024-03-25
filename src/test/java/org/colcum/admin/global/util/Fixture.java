3718e20d3dae3743aebad3b8205b3772bff92f0b
sirin0762 <tlsdydwls123@gmail.com>
2024-03-25 20:59:28 +0900

    test: 게시글 조회 테스트 적용

--- src/test/java/org/colcum/admin/global/util/Fixture.java
+++ src/test/java/org/colcum/admin/global/util/Fixture.java
@@ -1,21 +1,24 @@
 package org.colcum.admin.global.util;
 
-import org.colcum.admin.domain.post.domain.PostCategory;
+import org.colcum.admin.domain.post.domain.CommentEntity;
+import org.colcum.admin.domain.post.domain.type.PostCategory;
 import org.colcum.admin.domain.post.domain.PostEntity;
-import org.colcum.admin.domain.post.domain.PostStatus;
+import org.colcum.admin.domain.post.domain.type.PostStatus;
 import org.colcum.admin.domain.user.domain.Branch;
 import org.colcum.admin.domain.user.domain.UserEntity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fixture {
 
-    public static PostEntity createFixture(String title, String content, UserEntity user) {
-        return createFixture(title, content, PostCategory.ANNOUNCEMENT, PostStatus.UNCOMPLETED, false, null, user);
+    public static PostEntity createFixturePost(String title, String content, UserEntity user) {
+        return createFixturePost(title, content, PostCategory.ANNOUNCEMENT, PostStatus.UNCOMPLETED, false, null, user, new ArrayList<>());
     }
 
-    public static PostEntity createFixture(String title, String content, PostCategory postCategory, PostStatus postStatus, boolean isBookmarked,LocalDate expiredDate, UserEntity user) {
-        return new PostEntity(title, content, postCategory, postStatus, isBookmarked, expiredDate, user);
+    public static PostEntity createFixturePost(String title, String content, PostCategory postCategory, PostStatus postStatus, boolean isBookmarked, LocalDate expiredDate, UserEntity user, List<CommentEntity> comments) {
+        return new PostEntity(title, content, postCategory, postStatus, isBookmarked, expiredDate, user, comments);
     }
 
     public static UserEntity createFixtureUser() {
