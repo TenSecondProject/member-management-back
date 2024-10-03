ff34e278cd7137aa8478fc50b5ab5883cad75b5a
Jin <tlsdydwls123@gmail.com>
2024-10-03 16:36:44 +0900

    test: redis test 설정 제외 처리

--- src/test/java/org/colcum/admin/global/config/RedisTestContainerConfiguration.java
+++ src/test/java/org/colcum/admin/global/config/RedisTestContainerConfiguration.java
@@ -12,12 +12,12 @@ public class RedisTestContainerConfiguration implements BeforeAllCallback {
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        GenericContainer redis = new GenericContainer(DockerImageName.parse(REDIS_IMAGE))
-            .withExposedPorts(REDIS_INTERNAL_PORT);
-        redis.start();
-        System.setProperty("spring.data.redis.host", redis.getHost());
-        System.setProperty("spring.data.redis.port", String.valueOf(redis.getMappedPort(REDIS_INTERNAL_PORT
-        )));
+//        GenericContainer redis = new GenericContainer(DockerImageName.parse(REDIS_IMAGE))
+//            .withExposedPorts(REDIS_INTERNAL_PORT);
+//        redis.start();
+//        System.setProperty("spring.data.redis.host", redis.getHost());
+//        System.setProperty("spring.data.redis.port", String.valueOf(redis.getMappedPort(REDIS_INTERNAL_PORT
+//        )));
     }
 
 }
