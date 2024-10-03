8a98e8fc1c9149468e239196816adc64c51db91f
Jin <60607880+sirin0762@users.noreply.github.com>
2024-10-03 15:43:27 +0900

    Feature/#51 implement main announcement api (#53)
    * chore: redis test를 위한 testContainer 의존성 추가
    
    * feat: 공지사항 등록 API 구현
    
    공지사항 등록 API를 구현했다. 모든 게시글에 보이기 때문에 DB를 찌르는건 비효율적이라 판단되어 redis로 관리한다.
    
    * chore: yaml 설정 변경
    
    베타 테스트를 진행하고 있기 떄문에 개발계 ddl-auto를 none으로 변경한다.
    
    * chore: test redis 관련 설정 변경

--- src/test/java/org/colcum/admin/global/config/RedisTestContainerConfiguration.java
+++ src/test/java/org/colcum/admin/global/config/RedisTestContainerConfiguration.java
@@ -7,16 +7,17 @@ import org.testcontainers.utility.DockerImageName;
 
 public class RedisTestContainerConfiguration implements BeforeAllCallback {
 
-    private static final String REDIS_IMAGE = "redis:7.0.8-alpine";
-    private static final int REDIS_PORT = 6379;
+
+    private static final String REDIS_IMAGE = "redis:7.4.0-alpine";
+    private static final int REDIS_INTERNAL_PORT = 6379;
 
     @Override
     public void beforeAll(ExtensionContext context) {
         GenericContainer redis = new GenericContainer(DockerImageName.parse(REDIS_IMAGE))
-            .withExposedPorts(REDIS_PORT);
+            .withExposedPorts(REDIS_INTERNAL_PORT);
         redis.start();
         System.setProperty("spring.data.redis.host", redis.getHost());
-        System.setProperty("spring.data.redis.port", String.valueOf(redis.getMappedPort(REDIS_PORT
+        System.setProperty("spring.data.redis.port", String.valueOf(redis.getMappedPort(REDIS_INTERNAL_PORT
         )));
     }
 
